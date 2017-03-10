2e465cc2f10370ca4240fcf0694aada2d0a6a124
DK Do <dangkhoado259@gmail.com>
2017-03-10 14:11:32 -0500

    Add initial position model

--- plannr/position/models.py
+++ plannr/position/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+    hourly_salary = models.IntegerField()
+    department = models.CharField(max_length=100)
