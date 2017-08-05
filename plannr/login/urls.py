1912f171ee370961f1b4a8e3d448b4839d8d8563
nicholasjhs <n.hughsam@gmail.com>
2017-08-05 16:56:18 -0400

    Saturday august 5th session, refactor code, api, webpack resolved

--- plannr/login/urls.py
+++ plannr/login/urls.py
@@ -1,7 +1,10 @@
 from . import views
+from . import api
 from django.conf.urls import url
 
 app_name = 'login'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^loginaction/', api.SignInRequest.as_view()),
+    url(r'^signupaction/', api.SignUpRequest.as_view()),
 ]
--- plannr/static/src/login.js
+++ plannr/static/src/login.js
@@ -6,10 +6,14 @@ class Login extends React.Component {
 		super(props);
 		this.state = {
 			username: '',
-			password: ''
+			password: '',
+			showSignupForm: false,
 		};
 
 		this.attemptLogin = this.attemptLogin.bind(this);
+		this.passwordChange = this.passwordChange.bind(this);
+		this.signupFormAppear = this.signupFormAppear.bind(this);
+		this.usernameChange = this.usernameChange.bind(this);
 	}
 
 	attemptLogin() {
@@ -17,15 +21,17 @@ class Login extends React.Component {
 			username: this.state.username,
 			password: this.state.password
 		};
-		alert(this.props.url);
 		$.ajax({
                 type: 'POST',
-                url: this.props.url,
+                url: this.props.submit_url,
                 data: loginInfo,
                 datatype: 'json',
                 cache: false,
                 success: function(){
                     alert("Success!");
+                }.bind(this),
+                error: function(){
+                	alert("Failed!")
                 }.bind(this)
             })
         this.setState({username: '', password: ''});
@@ -33,6 +39,12 @@ class Login extends React.Component {
 
 	passwordChange(event) {
 		this.setState({password: event.target.value});
+	}
+
+	signupFormAppear() {
+		console.log('caca');
+		this.setState({showSignupForm: true});
+		console.log(this.state.showSignupForm);
 	}
 
 	usernameChange(event) {
@@ -43,25 +55,96 @@ class Login extends React.Component {
 	render() {
 		return (
 			<div className="login-content-container">
-				<div className="login-input-container" id="login-input-left">
-					<form onSubmit={this.attemptLogin}>
+				<form onSubmit={this.attemptLogin} className="login-input-container" id="login-input-left">
 						<div className="login-info-container">
+							<img src="" alt="LOGO" className="logo-resize"></img>
 							<Username usernameChange={this.usernameChange} />
 							<Passwrd passwordChange={this.passwordChange} />
 							<SubmitBtn />
 							<div className="login-signup-link-container">
-								<a href="" className="login-pwd-forgot-link" name="pwd_forget_link">Forgot your password?</a>
-								<a href="" className="login-signup-link" name="signup_link">Sign Up</a>
+								<p className="login-pwd-forgot-link login-links" name="pwd_forget_link">Forgot your password?</p>
+								<p className="login-signup-link login-links" onClick={this.signupFormAppear} name="signup_link">Sign Up</p>
 							</div>
 						</div>
-					</form>
-				</div>
-				<div className="login-input-container">
-					<div className="login-logo-container">
-						<img src="" alt="LOGO"></img>
-					</div>
-				</div>
+				</form>
+				<Signup showForm={this.state.showSignupForm} />
 			</div>
+		);
+	}
+}
+
+class Signup extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstname: '',
+			lastname: '',
+			email: '',
+			password: '',
+			organization: '',
+			show: props.showForm
+		};
+
+		this.attemptSignup = this.attemptSignup.bind(this);
+	}
+
+	attemptSignup() {
+		var signupInfo = {
+			firstname: this.state.firstname,
+			lastname: this.state.lastname,
+			email: this.state.email,
+			password: this.state.password,
+			organization: this.state.organization
+		};
+		$.ajax({
+			type: 'POST',
+			url: this.props.signup_url,
+			data: signupInfo,
+			datatype: 'json',
+			cache: false,
+			success: function(){
+				alert("SUCCESS!");
+				this.setState({show: false});
+			}.bind(this),
+			error: function(){
+
+			}
+		})
+	}
+
+	firstnameChange(event) {
+		this.setState({firstname: event.target.value});
+	}
+
+	lastnameChange(event) {
+		this.setState({lastname: event.target.value});
+	}
+
+	emailChange(event) {
+		this.setState({email: event.target.value});
+	}
+
+	spasswordChange(event) {
+		this.setState({password: event.target.value});
+	}
+
+	organizationChange(event) {
+		this.setState({organization: event.target.value});
+	}
+
+	render() {
+		return (
+				<form onSubmit={this.attemptSignup} className="login-input-container">
+					<div className={this.state.show ? "login-signup-container" : "login-signup-container hidden"}>
+						<h1 className="login-signup-title">SIGN UP</h1>
+						<SignupFirstName firstnameChange={this.firstnameChange} />
+						<SignupLastName lastnameChange={this.lastnameChange} />
+						<SignupEmail emailChange={this.emailChange} />
+						<SignupPassword spasswordChange={this.spasswordChange} />
+						<SignupOrganization organizationChange={this.organizationChange} />
+						<SignupBtn />
+					</div>
+				</form>
 		);
 	}
 }
@@ -69,7 +152,55 @@ class Login extends React.Component {
 class Passwrd extends React.Component {
 	render() {
 		return (
-			<input type="password" className="login-signin-inputs" onChange={this.props.passwordChange} name="signin_pwd" placeholder="Password" required></input>
+			<input type="password" className="login-page-inputs" onChange={this.props.passwordChange} name="signin_pwd" placeholder="Password" required></input>
+		);
+	}
+}
+
+class SignupFirstName extends React.Component {
+	render() {
+		return (
+			<input type="text" className="login-page-inputs" name="signup_firstname" placeholder="First Name" required></input>
+		);
+	}
+}
+
+class SignupLastName extends React.Component {
+	render() {
+		return (
+			<input type="text" className="login-page-inputs" name="signup_lastname" placeholder="Last Name" required></input>
+		);
+	}
+}
+
+class SignupEmail extends React.Component {
+	render() {
+		return (
+			<input type="text" className="login-page-inputs" name="signup_email" placeholder="Email / Username" required></input>
+		);
+	}
+}
+
+class SignupPassword extends React.Component {
+	render() {
+		return (
+			<input type="password" className="login-page-inputs" name="signup_password" placeholder="New Password" required></input>
+		);
+	}
+}
+
+class SignupOrganization extends React.Component {
+	render() {
+		return (
+			<input type="text" className="login-page-inputs" name="signup_organization" placeholder="Organization"></input>
+		);
+	}
+}
+
+class SignupBtn extends React.Component {
+	render() {
+		return (
+			<button type="submit" className="btn login-page-btn" name="signup_btn">SIGN UP</button>
 		);
 	}
 }
@@ -77,7 +208,7 @@ class Passwrd extends React.Component {
 class SubmitBtn extends React.Component {
 	render() {
 		return (
-			<button type="submit" className="btn login-connect-btn" name="connect_btn">LOGIN</button>
+			<button type="submit" className="btn login-page-btn" name="connect_btn">LOGIN</button>
 		);
 	}
 }
@@ -85,7 +216,7 @@ class SubmitBtn extends React.Component {
 class Username extends React.Component {
 	render() {
 		return (
-			<input type="text" className="login-signin-inputs active" onChange={this.props.usernameChange} name="signin_email" placeholder="Email@domain.com" required></input>
+			<input type="text" className="login-page-inputs" onChange={this.props.usernameChange} name="signin_email" placeholder="Email@domain.com" required></input>
 		);
 	}
 }
@@ -93,7 +224,9 @@ class Username extends React.Component {
 
 ReactDOM.render(
 	React.createElement(Login, {
-		url: '/login/'
+		url: '/login/',
+		submit_url: '/login/?loginaction/',
+		signup_url: 'login/?signupaction/'
 	}),
 	document.getElementById("login_react")
 );
