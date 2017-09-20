489e0a9d34882db880edc4baa4c9d4c1e5ec80a4
DK Do <dangkhoado259@gmail.com>
2017-09-20 14:11:23 -0400

    Add end_date field

--- plannr/event/models.py
+++ plannr/event/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from datetime import timedelta
 
 from django.db import models
 
@@ -6,6 +7,7 @@ class Event(models.Model):
     title = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     start_date = models.DateTimeField()
+    end_date = models.DateTimeField(null=True)
 
     class Meta:
         ordering = ('created',)
