693993f40a44419f843a68ff7791a79cc4d28370
DK Do <dangkhoado259@gmail.com>
2017-11-10 11:59:53 -0500

    Make organization name unique

--- plannr/organization/models.py
+++ plannr/organization/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Organization(models.Model):
-    organization_name = models.TextField(null=False)
+    organization_name = models.TextField(null=False, unique=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
