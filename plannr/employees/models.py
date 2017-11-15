e6d04dc334a9d9d5c09d60a41329c08579edd4b5
DK Do <dangkhoado259@gmail.com>
2017-11-15 13:18:15 -0500

    Fix manager col related name in employees table

--- plannr/employees/models.py
+++ plannr/employees/models.py
@@ -4,8 +4,11 @@ from django.db import models
 
 
 class Employees(models.Model):
-    employee = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
-    manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, related_name='employee')
+    employee = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                    on_delete=models.CASCADE, primary_key=True)
+    manager = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE, null=False,
+                                related_name='manager')
 
     class Meta:
         unique_together = ['manager', 'employee']
