4eb22b617ee7230cbc3a28efa71d345945b6e9fc
nicholasjhs <n.hughsam@gmail.com>
2017-11-22 16:29:33 -0500

    Created the stat model and made it work during login

---
+++ plannr/stats/models.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+from django.conf import settings
+from django.db import models
+
+class Stat(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True, on_delete=models.CASCADE)
+    hours = models.FloatField(default=0.0)
+    taken_shifts = models.IntegerField(default=0)
+    given_shifts = models.IntegerField(default=0)
+    total = models.FloatField(default=0)
