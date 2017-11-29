e586fd83afe06da6a0e827328cf8a11347bfced7
DK Do <dangkhoado259@gmail.com>
2017-11-29 12:09:52 -0500

    Redirect logged in users to home page

--- plannr/login/urls.py
+++ plannr/login/urls.py
@@ -1,10 +1,11 @@
+from django.contrib.auth import views as auth_views
 from . import views
 from . import api
 from django.conf.urls import url
 
 app_name = 'login'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', auth_views.login, {'template_name': 'registration/login.html', 'redirect_authenticated_user': True}, name='index'),
     url(r'^loginaction/', api.SignInRequest.as_view()),
     url(r'^signupaction/', api.SignUpRequest.as_view()),
     url(r'^logoutaction/', api.SignOutRequest.as_view()),
--- plannr/plannr/settings.py
+++ plannr/plannr/settings.py
@@ -145,6 +145,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+LOGIN_REDIRECT_URL = '/'
+
 REACT_ROUTES = [
     'week',
     'about',
